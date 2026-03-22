bc27bd47de4555f772e6516f6fd32258231cecd0
vladislav <107300120+homelless@users.noreply.github.com>
2026-03-22 10:18:04 +0300

    Initial Commit

---
+++ ASVAB/ASVABApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASVABApp.swift
+//  ASVAB
+//
+//  Created by MacBookAir on 22.03.26.
+//
+
+import SwiftUI
+
+@main
+struct ASVABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
